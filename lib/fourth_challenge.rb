ff082154593bef89614b1c6d6dedcdaa5b677b9c
Christine Cua <cloudycua@gmail.com>
2018-02-27 19:43:06 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,12 +1,13 @@
 def fourth_challenge
+
   epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-         {name: "Benvolio", age: "17", attitude: "worried"},
-         {name: "Mercutio", age: "18", attitude: "hot-headed"}
+         {name: "Mercutio", age: "18", attitude: "hot-headed"},
+         {name: "Benvolio", age: "17", attitude: "worried"}
       ]
    },
    :capulet => {
@@ -18,8 +19,7 @@ def fourth_challenge
           {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
- }
-}
+  }
 
 
 
